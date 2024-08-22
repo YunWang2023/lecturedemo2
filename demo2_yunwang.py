3a74ad27364b3f41cbb0076eb984257d65604f25
YunWang2023 <Yun.Wang@metropolia.fi>
2024-08-22 11:24:29 +0300

    demo is done

--- demo2_yunwang.py
+++ demo2_yunwang.py
@@ -1,3 +1,24 @@
+
+
+
+num1 = int(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+sum = num1 + num2
+
+print(sum)
+
+
+name = input("Enter your name: ")
+school = input("Enter your school: ")
+
+floatVariable = 4.234567854
+
+print("You are: ", name, "and your school name is: ", school)
+print(f" Your name is : {name} , and your school name is: {school},and your float Variable is : {floatVariable}  ")
+print(f" Your name is : {name} , and your school name is: {school},and your float Variable is : {floatVariable:.3f}  ")
+
+'''
+
 intVariable=4
 floatVariable=4.5
 stringVariable="software 1 is fun"
@@ -13,3 +34,12 @@ print(type(stringVariable))
 #typecasting is fun
 intVariable=int(floatVariable)
 print("here is the int version of float variable " , intVariable)
+
+
+
+share0floan=5000.50/3
+print(share0floan)
+print(int(share0floan))
+print(type(share0floan))
+
+'''
