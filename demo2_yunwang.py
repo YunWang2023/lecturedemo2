8692e03d65417cdcf6405213c3206ca4a5700a6a
YunWang2023 <Yun.Wang@metropolia.fi>
2024-08-22 10:46:29 +0300

    ok

--- demo2_yunwang.py
+++ demo2_yunwang.py
@@ -1,1 +1,15 @@
-print(123)
+intVariable=4
+floatVariable=4.5
+stringVariable="software 1 is fun"
+
+print(intVariable)
+print(floatVariable)
+print(stringVariable)
+
+print(type(intVariable))
+print(type(floatVariable))
+print(type(stringVariable))
+
+#typecasting is fun
+intVariable=int(floatVariable)
+print("here is the int version of float variable " , intVariable)
